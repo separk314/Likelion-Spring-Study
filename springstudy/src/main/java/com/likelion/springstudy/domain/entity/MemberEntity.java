f4f362c367570ba8eb02b67b0e054a27451a357e
Soeun Park <separk314@naver.com>
2023-09-13 23:33:36 +0900

    week 2

--- springstudy/src/main/java/com/likelion/springstudy/domain/entity/MemberEntity.java
+++ springstudy/src/main/java/com/likelion/springstudy/domain/entity/MemberEntity.java
@@ -1,35 +1,33 @@
 package com.likelion.springstudy.domain.entity;
 
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter @Setter
+@Table(name = "member") // 테이블명을 명시하지 않으면 클래스명(Member_Entity)을 테이블명으로 사용
 public class MemberEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
 
     @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
     private String nickname;
 
-    private int age;
-
-    private boolean isAdult;
-
-    public MemberEntity(Long id, String name, String nickname, int age) {
+    @Builder
+    public MemberEntity(Long id, String username, String password, String nickname) {
         this.id = id;
-        this.name = name;
+        this.username = username;
+        this.password = password;
         this.nickname = nickname;
-        this.age = age;
-        this.isAdult = age >= 20;
     }
 }
