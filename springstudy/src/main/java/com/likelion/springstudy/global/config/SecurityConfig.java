2a9d209d76776300d75c03319f1752055d44b772
soeun <separk314@naver.com>
2023-10-11 21:03:58 +0900

    feat: security setting

--- springstudy/src/main/java/com/likelion/springstudy/global/config/SecurityConfig.java
+++ springstudy/src/main/java/com/likelion/springstudy/global/config/SecurityConfig.java
@@ -1,7 +1,37 @@
 package com.likelion.springstudy.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
+
+
+    // CSRF 설정
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and().build();
+    }
+
+    // CORS 설정
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
--- springstudy/src/main/java/com/likelion/springstudy/global/config/TimezoneConfig.java
+++ springstudy/src/main/java/com/likelion/springstudy/global/config/TimezoneConfig.java
@@ -1,4 +1,17 @@
 package com.likelion.springstudy.global.config;
 
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@Configuration
 public class TimezoneConfig {
+
+    private static final String KST = "Asia/Seoul";
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(KST));
+    }
 }
